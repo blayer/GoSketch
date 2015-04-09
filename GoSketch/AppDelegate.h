957de437e345898a5fa46521d8406188f3f974ca
Changkun Zhao <changkunzhao@Changkuns-Air.home>
2015-04-09 12:05:00 -0400

    Initial Commit

---
+++ GoSketch/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GoSketch
+//
+//  Created by Changkun Zhao on 4/9/15.
+//  Copyright (c) 2015 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
