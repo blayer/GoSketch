54bcf7b758707a9daf6b384ea76c89f82bbccc52
Changkun Zhao <changkunzhao@Changkuns-Air.home>
2015-04-09 12:24:25 -0400

    add new files

---
+++ Pods/Target Support Files/Pods-GoSketch-iOS-Color-Picker/Pods-GoSketch-iOS-Color-Picker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoSketch_iOS_Color_Picker : NSObject
+@end
+@implementation PodsDummy_Pods_GoSketch_iOS_Color_Picker
+@end
---
+++ Pods/Target Support Files/Pods-GoSketch/Pods-GoSketch-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// iOS-Color-Picker
+#define COCOAPODS_POD_AVAILABLE_iOS_Color_Picker
+#define COCOAPODS_VERSION_MAJOR_iOS_Color_Picker 1
+#define COCOAPODS_VERSION_MINOR_iOS_Color_Picker 1
+#define COCOAPODS_VERSION_PATCH_iOS_Color_Picker 3
+
